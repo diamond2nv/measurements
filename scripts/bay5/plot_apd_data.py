f720e0a78f98bfe9ce9304d980b3080f6f2a81f0
Cristian Bonato <c.bonato@hw.ac.uk>
2018-02-02 12:09:19 +0000

    -CB

--- scripts/bay5/plot_apd_data.py
+++ scripts/bay5/plot_apd_data.py
@@ -3,13 +3,12 @@ import numpy as np
 import pylab as plt
 
 #load the array of counts
-a = np.load('C:/Research/test.npz')
+a = np.load('C:/Research/HDscan3.npz')
 counts = a['counts']
 xPos = a['xPos']
 yPos = a['yPos']
 
 #plot is using matplotlib
-plt.pcolor(xPos, yPos, data)
-plt.axis('equal')
+plt.pcolor(xPos, yPos, counts)
 plt.colorbar()
 plt.show()
