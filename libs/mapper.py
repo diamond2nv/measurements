374225f222595a3064f8f735760f5d1292c4d701
Raphael Proux <raphaelproux@gmail.com>
2018-03-19 18:26:15 +0000

    Last architecture update with integration of single axes handling for the scans and streamlined scanner classes. Not tested in the lab yet but using a test scanner class only. -RProux

--- libs/mapper.py
+++ libs/mapper.py
@@ -9,11 +9,12 @@ import pylab as pl
 import time
 from tools import data_object as DO
 import numpy as np
+from measurements.libs.mapper_scanners import move_smooth
 
 
 class XYScan ():
-    def __init__(self, scanner=None, detectors=None):
-        self._scanner = scanner
+    def __init__(self, scanner_axes=None, detectors=None):
+        self._scanner_axes = scanner_axes
         self._detectors = detectors
 
         self.delayBetweenPoints = 1
@@ -23,19 +24,11 @@ class XYScan ():
 
         self._back_to_zero = False
 
-        # Check what kind of detector is used (spectrometer or apd)
-        # if (isinstance (detector, PylonNICtrl) or isinstance (detector, ActonLockinCtrl) or isinstance (detector, ActonNICtrl)):
-        #     self.detector_type = 'spectro'
-        # elif (isinstance (detector, APDCounterCtrl)):
-        #     self.detector_type = 'apd'
-        # else:
-        #     self.detector_type = 'unknown'
-
         # determine the longest test delay in the detectors
         self.max_delay_state_check = max([detector.delay_state_check for detector in self._detectors])
         self.max_delay_after_readout = max([detector.delay_after_readout for detector in self._detectors])
 
-    def set_delays (self, between_points, between_rows):
+    def set_delays(self, between_points, between_rows):
         self.delayBetweenPoints = between_points
         self.delayBetweenRows = between_rows
 
@@ -43,8 +36,7 @@ class XYScan ():
         # to go back to 0 V at the end of a scan
         self._back_to_zero = True
 
-
-    def set_range (self, xLims, xStep, yLims=None, yStep=None):
+    def set_range(self, xLims, xStep, yLims=None, yStep=None):
 
         self.xNbOfSteps = int(abs(pl.floor((float(xLims[1]) - float(xLims[0])) / float(xStep))) + 1)
         self.xPositions = pl.linspace(xLims[0], xLims[1], self.xNbOfSteps)
@@ -62,54 +54,52 @@ class XYScan ():
         if self._detectors is None:
             self.counts = None
         else:
-            self.counts = [pl.zeros ([self.xNbOfSteps, self.yNbOfSteps]) for detector in self._detectors]
-            #print(self.counts)
-        
+            self.counts = [pl.zeros([self.xNbOfSteps, self.yNbOfSteps]) for detector in self._detectors]
+            # print(self.counts)
 
     def set_trigger(self, trigger=True, feedback=True):
         self.trigger_active = trigger
         self.feedback_active = feedback
 
-    def secondsInHMS(self, nbOfSeconds):
+    def seconds_in_HMS(self, nbOfSeconds):
         hours = pl.floor(nbOfSeconds / 3600)
         minutes = pl.floor(nbOfSeconds % 3600 / 60)
         seconds = nbOfSeconds - minutes*60 - hours*3600
         return hours, minutes, seconds
 
-    def print_elapsed_time (self, startTime, currentIndex, totalNbOfSteps):
+    def print_elapsed_time(self, startTime, currentIndex, totalNbOfSteps):
         elapsedTime = float(time.time() - startTime)
         remainingTime = elapsedTime / currentIndex * (totalNbOfSteps - (currentIndex-1))
         
-        hoursE, minutesE, secondsE = self.secondsInHMS(elapsedTime)
-        hoursR, minutesR, secondsR = self.secondsInHMS(remainingTime)
+        hoursE, minutesE, secondsE = self.seconds_in_HMS(elapsedTime)
+        hoursR, minutesR, secondsR = self.seconds_in_HMS(remainingTime)
 
         print('Elapsed time: {:.0f} h {:.0f} min {:.0f} s\tRemaining time: {:.0f} h {:.0f} min {:.0f} s'.format(hoursE, minutesE, secondsE, hoursR, minutesR, secondsR))
 
     def wait_first_point(self, detectors):
-        if detectors is None:
-            pass
-        else:
+        if detectors is not None:
             while not all([detector.first_point() for detector in detectors]):
                 time.sleep(self.max_delay_state_check)
 
     def wait_for_ready(self, detectors):
-        if detectors is None:
-            pass
-        else:
+        if detectors is not None:
             while not all([detector.is_ready() for detector in detectors]):
                 time.sleep(self.max_delay_state_check)
 
     def init_detectors(self, detectors):
-        if detectors is None:
-            pass
-        else:
+        if detectors is not None:
             for detector in detectors:
                 detector.initialize()
 
-    def run_scan (self):
+    def init_scanners(self, scanner_axes):
+        if scanner_axes is not None:
+            for scanner_axis in scanner_axes:
+                scanner_axis.initialize()
+
+    def run_scan(self):
         try:
             self.init_detectors(self._detectors)
-            self._scanner.initialize()
+            self.init_scanners(self._scanner_axes)
 
             print('Total number of steps: {}\n'.format(self.totalNbOfSteps) +
                   'X number of steps:     {}\n'.format(self.xNbOfSteps) +
@@ -119,7 +109,7 @@ class XYScan ():
             first_point = True
             idx = 0
 
-            self._scanner.move_smooth(targets=[self.xPositions[0], self.yPositions[0]])
+            move_smooth(self._scanner_axes, targets=[self.xPositions[0], self.yPositions[0]])
 
             print('\nScanners are at start position. Waiting for spectrometer acquisition.\n')
 
@@ -131,8 +121,8 @@ class XYScan ():
                 for id_x, x in enumerate(self.xPositions):
                     idx += 1
                     
-                    self._scanner.moveX(x)
-                    self._scanner.moveY(y)
+                    self._scanner_axes[0].move(x)
+                    self._scanner_axes[1].move(y)
 
                     # display update
                     print('{}/{} \t{:.1f} \t{:.1f}'.format(idx, self.totalNbOfSteps, x, y))
@@ -167,12 +157,13 @@ class XYScan ():
 
                 # move back to first point of row smoothly
                 if y != self.yPositions[-1]:
-                    self._scanner.move_smooth(targets=[self.xPositions[0], y])
+                    self._scanner_axes[0].move_smooth(target=self.xPositions[0])
 
             # go smoothly to start position
             if self._back_to_zero:
                 print('\nGoing back to 0 V on scanners...')
-                self._scanner.move_smooth(targets=[0, 0])
+
+                move_smooth(self._scanner_axes, targets=[0, 0])
 
             print('\nSCAN COMPLETED\n' +
                   'X from {:.2f} V to {:.2f} V with step size {:.2f} V (nb of steps: {})\n'.format(self.xPositions[0], self.xPositions[-1], self.xStep, self.xNbOfSteps) +
@@ -182,7 +173,8 @@ class XYScan ():
         except KeyboardInterrupt:
             print('\n####  Program interrupted by user.  ####')
         finally:
-            self._scanner.close()
+            for scanner in self._scanners:
+                scanner.close()
             for detector in self._detectors:
                 detector.close()
 
@@ -193,13 +185,13 @@ class XYScan ():
         print("File saved")
 
     def save_to_npz(self, file_name):
-        np.savez (file_name+'.npz', self.counts)
+        np.savez(file_name+'.npz', self.counts)
 
     def plot_counts(self):
 
         if (self.detector_type == 'apd'):
             pl.figure(figsize=(10, 10))
-            pl.pcolor (self.counts)
+            pl.pcolor(self.counts)
             pl.show()
         else:
             print("No counts available.. use APD")
--- libs/mapper_scanners.py
+++ libs/mapper_scanners.py
@@ -19,175 +19,232 @@ if sys.version_info.major == 3:
 reload(NIBox)
 reload(mgen)
 
+#################################################
+#           DEFAULT SCANNERS CLASSES            #
+#################################################
+# These classes should not be modified unless you know what you are doing
+# For Scanner implementation, see the LAB SCANNERS CLASSES section below.
 
 class ScannerCtrl (mgen.DeviceCtrl):
 
-    def __init__(self):
+    """Default Scanner class which defines the mandatory functions to be present in any
+    scanner class and gives them default behaviour. Also contains functions like 
+    move_smooth() which should mostly work the same way for any type of scanner
+    
+    Attributes:
+        number_of_axes (int): number of axes/channels of the device
+        smooth_delay (float): delay in seconds between each step of the move_smooth
+        smooth_step (float): amplitude of each step for the move_smooth function
+        string_id (str): string identifying the device. Used in error messages.
+    """
+    
+    def __init__(self, channels=[]):
         self.smooth_step = 1.
         self.smooth_delay = 0.05
         self.string_id = 'Unknown scanner'
-        self._channels = []
 
-    def initialize(self):
+        if channels is None:
+            self.number_of_axes = 0
+        else:
+            try:
+                self.number_of_axes = len(channels)
+            except TypeError:
+                self.number_of_axes = len(list(channels))
+
+        try:
+            channels = list(channels)
+        except TypeError:
+            channels = [channels]
+        if channels == [None]:
+            channels = []
+
+        self._channels = channels
+        self.number_of_axes = len(channels)
+        self._dev_initialised = False
+        self._dev_closed = True
+
+    def __getitem__(self, key):
+        if type(key) is not int:
+            raise TypeError('Axis number should be an integer.')
+        if not 0 <= key < self.number_of_axes:
+            raise IndexError(f'The addressed axis does not exist (out of bounds). This device has {self.number_of_axes} axes defined, you tried accessing axis {key}.')
+        return Saxis(self, key)
+
+    def __len__(self):
+        return self.number_of_axes
+
+    def initialize(self, force=False):
+        if force:
+            self._dev_initialised = False
+        if not self._dev_initialised:
+            try:
+                self._initialize()
+            except visa.VisaIOError as err:
+                self.visa_error_handling(err)
+            self._dev_initialised = True
+            self._dev_closed = False
+
+    def _initialize(self):
         pass
 
     def move(self, target, axis=0):
-        pass
+        if 0 <= axis < self.number_of_axes:
+            self._move(target, axis)
 
-    def moveX(self, value):
-        pass
-
-    def moveY(self, value):
+    def _move(self, target, axis=0):
         pass
 
     def get(self, axis=0):
+        if 0 <= axis < self.number_of_axes:
+            return self._get(axis)
+        else:
+            return None
+
+    def _get(self, axis=0):
         return None
 
-    def getX (self):
-        pass
+    def _close(self):
+        # here, call device close function
+        self._dev_closed = True
 
-    def getY (self):
-        pass
+    def close(self):
+        if not self._dev_closed:
+            super().close()
 
     def set_smooth_delay(self, value):
         self.smooth_delay = value
 
     def set_smooth_step(self, value):
         self.smooth_step = value
-
-    def _close(self):
-        pass
-
-    def move_smooth(self, targets=[0], axes=None):
-        # targets and axes should be iterables (or axes=None if default x, y, etc.)
-        if axes is None:
-            axes = range(len(targets))
-        to_pos_list = targets
-        nb_steps_list = []
-        from_pos_list = []
-        for axis, to_pos in zip(axes, to_pos_list):
-            from_pos = self.get(axis=axis)
-            from_pos_list.append(from_pos)
-            #print(from_pos,to_pos,axis)
-            if from_pos is None:
-                nb_steps_list.append(0)
-            else:
-                nb_steps_list.append(int(abs(pl.floor((from_pos - to_pos) / float(self.smooth_step))) + 1))
-
-        total_nb_of_steps = max(nb_steps_list)
-        smooth_positions = []
-        for to_pos, from_pos, nb_steps in zip(to_pos_list, from_pos_list, nb_steps_list):
-            if from_pos is None:
-                from_pos = 0
-            smooth_positions.append(pl.append(pl.linspace(from_pos, to_pos, nb_steps), 
-                                              pl.zeros(total_nb_of_steps - nb_steps) + to_pos))
-
-        for i in range(total_nb_of_steps):
-            for axis, pos in zip(axes, smooth_positions):
-                self.move(pos[i], axis=axis)
-            time.sleep(self.smooth_delay)
             
     def close_error_handling(self):
         print('WARNING: Scanners {} did not close properly.'.format(self.string_id))
 
+class Saxis():
+    def __init__(self, scanner, axis):
+        self.axis = axis
+        self.scanner = scanner
+    def __getitem__(self, key):
+        raise AttributeError("'Saxis' object is not subscriptable.")
+    def move(self, target):
+        self.scanner.move(target=target, axis=self.axis)
+    def get(self):
+        return self.scanner.get(axis=self.axis)
+    def move_smooth(self, target):
+        move_smooth(scanner_axes=[self], targets=[target])
+    def initialize(self):
+        self.scanner.initialize()
+    def close(self):
+        self.scanner.close()
+
+def move_smooth(scanner_axes, targets=[]):
+    smooth_steps = [s_axis.scanner.smooth_step for s_axis in scanner_axes]
+    smooth_delay = 0
+    for s_axis in scanner_axes:
+        if s_axis.scanner.smooth_delay > smooth_delay:
+            smooth_delay = s_axis.scanner.smooth_delay
+
+    to_pos_list = targets
+    nb_steps_list = []
+    from_pos_list = []
+    for s_axis, smooth_step, to_pos in zip(scanner_axes, smooth_steps, to_pos_list):
+        from_pos = s_axis.get()
+        from_pos_list.append(from_pos)
+        # print(from_pos,to_pos,axis)
+        if from_pos is None:
+            nb_steps_list.append(0)
+        else:
+            nb_steps_list.append(int(abs(pl.floor((from_pos - to_pos) / float(smooth_step))) + 1))
+
+    total_nb_of_steps = max(nb_steps_list)
+    smooth_positions = []
+    for to_pos, from_pos, nb_steps in zip(to_pos_list, from_pos_list, nb_steps_list):
+        if from_pos is None:
+            from_pos = 0
+        smooth_positions.append(pl.append(pl.linspace(from_pos, to_pos, nb_steps), 
+                                          pl.zeros(total_nb_of_steps - nb_steps) + to_pos))
+
+    for i in range(total_nb_of_steps):
+        for s_axis, pos in zip(scanner_axes, smooth_positions):
+            s_axis.move(pos[i])
+        time.sleep(smooth_delay)
+
+#################################################
+#             LAB SCANNERS CLASSES              #
+#################################################
+
+
+class TestScanner (ScannerCtrl):
+
+    def __init__(self, address, channels=[2,3]):
+        super().__init__(channels=channels)
+        self.string_id = 'Test device'
+        self.address = address
+       
+        self.smooth_step = 0.5
+        self.smooth_delay = 0.5
+
+    def _initialize(self):
+        print('Device initialized')
+
+    def _move(self, target, axis=0):
+        print(f'Move axis {axis} to target {target}')
+
+    def _get(self, axis=0):
+        print(f'Got value from axis {axis}')
+        return 1.1
+
+    def _close(self):
+        print('Closing test device')
+
 
 class AttocubeNI (ScannerCtrl):
-
-    def __init__ (self, chX='/Weetabix/ao0', chY='/Weetabix/ao1', conversion_factor=1/15.):
-        super().__init__()
+    def __init__(self, chX='/Weetabix/ao0', chY='/Weetabix/ao1', conversion_factor=1/15.):
+        super().__init__(channels=[chX, chY])
         self.string_id = 'Attocube ANC scanners controlled by NI box DC AO'
-        self._channels = [chX, chY]
         self.conversion_factor = conversion_factor
 
         self.smooth_step = 1
         self.smooth_delay = 0.05
 
-        self._currX = 0
-        self._currY = 0
+        self._curr_pos = [0 for channel in self._channels]
 
-    def initialize (self):
-        self.scanners_volt_drive_X = voltOut(self._channels[0])
-        self.scanners_volt_drive_Y = voltOut(self._channels[1])
+    def _initialize(self):
+        self.scanners_volt_drives = [voltOut(self._channels[i]) for i in self._channels]
 
-    def move(self, target, axis=0):
-        if axis == 0:
-            self.moveX(target)
-        elif axis == 1:
-            self.moveY(target)
+    def _move(self, target, axis=0):
+        self.scanners_volt_drives[axis].write(self.conversion_factor * target)
+        self._curr_pos[axis] = target
 
-    def moveX (self, value):
-        self.scanners_volt_drive_X.write(self.conversion_factor * value)
-        self._currX = value
-
-    def moveY (self, value):
-        self.scanners_volt_drive_Y.write(self.conversion_factor * value)
-        self._currY = value
+    def _get(self, axis=0):
+        return self._curr_pos[axis]
 
     def _close(self):
-        self.scanners_volt_drive_X.StopTask()
-        self.scanners_volt_drive_Y.StopTask()
-
-    def get(self, axis=0):
-        if axis == 0:
-            return self.getX()
-        elif axis == 1:
-            return self.getY()
-
-    def getX (self):
-        return self._currX
-
-    def getY (self):
-        return self._currY
+        for scanner in self.scanners_volt_drives:
+            scanner.StopTask()
 
 
 class AttocubeVISA (ScannerCtrl):
 
-    def __init__ (self, VISA_address, axisX=1, axisY=2):
-        super().__init__()
+    def __init__(self, VISA_address, chX=1, chY=2):
+        super().__init__(channels=[chX, chY])
         self.string_id = 'Attocube ANC scanners controlled by VISA'
         self._VISA_address = VISA_address
-        self._channels = [axisX, axisY]
        
         self.smooth_step = 0.5
         self.smooth_delay = 0.05
 
-        self._currX = 0
-        self._currY = 0
+    def _initialize(self):
+        self._ANChandle = attoANC.AttocubeANC(self._VISA_address)
 
-    def initialize (self):
-        try:
-            self._ANChandle = attoANC.AttocubeANC(self._VISA_address)
-        except visa.VisaIOError as err:
-            self.visa_error_handling(err)
+    def _move(self, target, axis=0):
+        self._ANChandle.setOffset(target, self._channels[axis])
 
-    def move(self, target, axis=0):
-        if axis == 0:
-            self.moveX(target)
-        elif axis == 1:
-            self.moveY(target)
-
-    def moveX (self, value):
-        self._ANChandle.setOffset(value, self._channels[0])
-        self._currX = value
-
-    def moveY (self, value):
-        self._ANChandle.setOffset(value, self._channels[1])
-        self._currY = value
+    def _get(self, axis=0):
+        return self._ANChandle.getOffset(self._channels[axis])
 
     def _close(self):
         self._ANChandle.close()
-
-    def get(self, axis=0):
-        if axis == 0:
-            return self.getX()
-        elif axis == 1:
-            return self.getY()
-
-    def getX (self):
-        return self._ANChandle.getOffset(self._channels[0])
-        
-    def getY (self):
-        return self._ANChandle.getOffset(self._channels[1])
 
 
 # class AttocubeVISAstepper (ScannerCtrl):
@@ -207,7 +264,7 @@ class AttocubeVISA (ScannerCtrl):
         
 #         self._curr_position = [0, 0]
        
-#     def initialize (self):
+#     def _initialize (self):
 #         try:
 #             self._ANChandle = attoANC.AttocubeANC(self._VISA_address)
 #         except visa.VisaIOError as err:
@@ -263,25 +320,16 @@ class AttocubeVISA (ScannerCtrl):
 class Keithley2220(ScannerCtrl):
 
     def __init__(self, VISA_address, channels):
-        super().__init__()
+        super().__init__(channels)
         self.string_id = 'Keithley PSU2220 DC power supply controlled by VISA'
         self._VISA_address = VISA_address
 
         self.smooth_step = 0.5
         self.smooth_delay = 0.05
 
-        try:
-            channels = list(channels)
-        except TypeError:
-            channels = [channels]
-        if len(channels) == 1:
-            channels.append(None)
+        self._axes_modes = ['voltage' for ch in self._channels]
 
-
-        self._channels = channels
-        
-
-    def initialize(self, switch_on_output=False):
+    def _initialize(self, switch_on_output=False):
         try:
             self._Keithley_handle = KeithleyPSU2220.Keithley2220(self._VISA_address)
         except visa.VisaIOError as err:
@@ -289,41 +337,30 @@ class Keithley2220(ScannerCtrl):
         if switch_on_output:
             self.output_switch(on=True)
 
-    def move(self, value, axis, mode='voltage'):
-        if axis is not None:
-            if mode == 'voltage':
-                self._Keithley_handle.setVoltage(channel=self._channels[axis], voltage=value)
-            elif mode == 'current':
-                self._Keithley_handle.setCurrent(channel=self._channels[axis], voltage=value)
+    def set_axis_mode(self, axis, mode):
+        if axis is not None and mode in {'voltage', 'current'}:
+            self._axes_modes = mode
 
-    def moveX(self, value):
-        self.move(value=value, axis=0)
-    def moveY(self, value):
-        self.move(value=value, axis=1)
+    def _move(self, value, axis):
+        if self._axes_modes[axis] == 'voltage':
+            self._Keithley_handle.setVoltage(channel=self._channels[axis], voltage=value)
+        elif self._axes_modes[axis] == 'current':
+            self._Keithley_handle.setCurrent(channel=self._channels[axis], voltage=value)
 
-    def get_setting(self, axis, mode='voltage'):
+    def get_set_value(self, axis):
         if axis is not None:
-            if mode == 'voltage':
+            if self._axes_modes[axis] == 'voltage':
                 return self._Keithley_handle.readSetVoltage(channel=self._channels[axis])
-            elif mode == 'current':
+            elif self._axes_modes[axis] == 'current':
                 return self._Keithley_handle.readSetCurrent(channel=self._channels[axis])
         else:
             return None
 
-    def get(self, axis=None, mode='voltage'):
-        if axis is not None:
-            if mode == 'voltage':
-                return self._Keithley_handle.readVoltage(channel=self._channels[axis])
-
-            elif mode == 'current':
-                return self._Keithley_handle.readCurrent(channel=self._channels[axis]) 
-        else:
-            return None
-
-    def getX(self):
-        return self.get(axis=0)
-    def getY(self):
-        return self.get(axis=1)
+    def _get(self, axis=None):
+        if self._axes_modes[axis] == 'voltage':
+            return self._Keithley_handle.readVoltage(channel=self._channels[axis])
+        elif self._axes_modes[axis] == 'current':
+            return self._Keithley_handle.readCurrent(channel=self._channels[axis])
 
     def channel_combine(self, combMode=KeithleyPSU2220.Keithley2220channelModes.OFF):
         # 'off', 'parallel' or 'series'
@@ -340,25 +377,30 @@ class Keithley2220_neg_pos(Keithley2220):
     def __init__(self, VISA_address, ch_neg, ch_pos):
         super().__init__(VISA_address, channels=[ch_neg,ch_pos])
 
-    def move(self, value, axis=0):
-        if axis == 0:
-            if value < 0:
-                super().move(-value, axis=0)
-                super().move(0, axis=1)
-            else:
-                super().move(value, axis=1)
-                super().move(0, axis=0)
-
-    def get(self, axis=0):
-        if axis == 0:
-            neg_bias = self._Keithley_handle.readVoltage(channel=self._channels[0])
-            pos_bias = self._Keithley_handle.readVoltage(channel=self._channels[1])
-            return pos_bias-neg_bias
-        elif axis == 1:
-            return 0
+    def _move(self, value, axis=0):
+        if value < 0:
+            super().move(-value, axis=0)
+            super().move(0, axis=1)
         else:
-            return None
+            super().move(value, axis=1)
+            super().move(0, axis=0)
+
+    def _get(self, axis=0):
+        neg_bias = super().get(0)
+        pos_bias = super().get(1)
+        return pos_bias-neg_bias
 
 
+if __name__ == '__main__':
+    toto = TestScanner(address='totoLand', channels=[3,5])
+    toto.initialize()
+    print(toto[0].get())
+    print(toto[1].get())
+    toto[0].move(2)
+    toto[1].move(4)
+    # toto[0].move_smooth(3)
 
+    move_smooth(toto, [3,4])
 
+    for titi in toto:
+        titi.move(10)
