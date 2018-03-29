f6fb35dada10b81231efbaa1736388f4993f217c
QPL bay2 small computer <qphotonicslab@gmail.com>
2018-03-29 19:20:08 +0100

    Created attopos for bay 2 in master branch by mistake. Created voltageMap_acton-bay2.py in master branch by mistake. Mauro

---
+++ instruments/Lakeshore335.py
@@ -0,0 +1,33 @@
+
+import visa
+import types
+#import logging
+#import re
+#import math
+#import time
+
+class Lakeshore335():
+
+    def __init__(self, visaAddress):
+        rm = visa.ResourceManager()
+        self._instr = rm.open_resource(visaAddress)
+        self._instr.baud_rate = 57600
+        self._instr.data_bits = 7
+        self._instr.parity = visa.constants.Parity.odd
+        self._instr.stop_bits = visa.constants.StopBits.one
+
+    def __enter__(self):
+        return self
+    
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+    def id (self):
+        print(self._instr.ask ('*IDN?'))
+
+    def get_kelvin (self, channel):
+        return float(self._instr.ask ('KRDG? '+str(channel)))
+
+    def close(self):
+        self._instr.close()
+
