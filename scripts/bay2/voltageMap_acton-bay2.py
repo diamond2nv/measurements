0549ba90daa490d1180a925c4659a05ad7db7058
QPL bay2 small computer <qphotonicslab@gmail.com>
2018-05-16 11:40:16 +0100

    -CB

--- scripts/bay2/voltageMap_acton-bay2.py
+++ scripts/bay2/voltageMap_acton-bay2.py
@@ -25,7 +25,7 @@ reload(mdet)
 delayBetweenPoints = 0.5
 delayBetweenRows = 0.
 
-xLims = (40, 0)  # (0, 5)
+xLims = (0, 5)  # (0, 5)
 xStep = 0.5
 
 voltsDirectory = r'C:\Users\QPL\Desktop\temp_measurements'
@@ -33,10 +33,10 @@ voltsDirectory = r'C:\Users\QPL\Desktop\temp_measurements'
 #######################
 # instruments
 
-psuCtrl = mscan.Keithley2220(VISA_address=r'GPIB0::10::INSTR', channels=[1])
+#psuCtrl = mscan.Keithley2220(VISA_address=r'GPIB0::10::INSTR', channels=[1])
 
 
-#psuCtrl = mscan.Keithley2220_neg_pos(VISA_address=r'GPIB0::10::INSTR', ch_neg=1, ch_pos=2)
+psuCtrl = mscan.Keithley2220_neg_pos(VISA_address=r'GPIB0::10::INSTR', ch_neg=1, ch_pos=2)
 ##psuCtrl = mscan.Keithley2220_negpos(VISA_address=r'GPIB0::10::INSTR', ch_neg=1, ch_pos=2)
 psuCtrl.set_smooth_delay(0.5)
 
@@ -48,7 +48,7 @@ d = datetime.datetime.now()
 voltsFilePath = os.path.join(voltsDirectory, 'powerInVolts_{:%Y-%m-%d_%H-%M-%S}.txt'.format(d))
 
 # Scanning program
-volts_scan = mapper.XYScan(scanner_axes=[psuCtrl[0]], detectors=[spectroCtrl, multimeterCtrl])
+volts_scan = mapper.XYScan(scanner_axes=[psuCtrl[0]], detectors=[multimeterCtrl])
 #volts_scan = mapper.XYScan(scanner_axes=[psuCtrl[0]], detectors=[spectroCtrl])
 volts_scan.set_range(xLims=xLims, xStep=xStep)
 volts_scan.set_delays(between_points=delayBetweenPoints, between_rows=delayBetweenRows)
@@ -56,5 +56,5 @@ volts_scan.set_back_to_zero()
 
 volts_scan.run_scan()
 
-volts_scan.save_to_txt(voltsFilePath, array=volts_scan.counts[1])
+volts_scan.save_to_txt(voltsFilePath, array=volts_scan.counts)
 
